e91ab6963ec79a628f8620af3643eb2844de21d7
CAR0L1N <126708653+CAR0L1N@users.noreply.github.com>
2023-03-01 19:01:54 +0100

    Create Install-Grub-Customizer.sh

---
+++ Scripts/Install-Grub-Customizer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer &&
+sudo apt-get update &&
+sudo apt-get install grub-customizer
+
+#   All on one line!
+#   sudo add-apt-repository ppa:danielrichter2007/grub-customizer && sudo apt-get update && sudo apt-get install grub-customizer
