42b74f89901cc5b04dd1c9b324c43a490808c3b0
CAR0L1N <126708653+CAR0L1N@users.noreply.github.com>
2023-03-05 11:21:37 +0100

    Update FirstSteps.sh

--- Scripts/FirstSteps.sh
+++ Scripts/FirstSteps.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update &&
 sudo apt-get upgrade -y &&
-sudo apt-get install apache2 php php-mysql php-curl php-gd php-soap php-mbstring xrdp rdesktop automake autoconf git build-essential mariadb-server cmake libbz2-dev libace-dev libssl-dev libtool libmariadb-dev -y
+sudo apt-get install apache2 php php-mysql php-curl php-gd php-soap php-mbstring xrdp rdesktop automake autoconf git build-essential mariadb-server cmake libbz2-dev libace-dev libssl-dev libtool libmariadb-dev screen gdb g++ libgtkmm-3.0-dev gettext libssl-dev libarchive-dev -y
 
 #   All on one line!
 #   sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install sudo apt-get install apache2 php php-mysql php-curl php-gd php-soap php-mbstring xrdp rdesktop automake autoconf git build-essential mariadb-server cmake libbz2-dev libace-dev libssl-dev libtool libmariadb-dev -y
